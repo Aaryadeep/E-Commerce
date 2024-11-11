7f971ed969c6e5337517bf4749d3cd2b34ddfc12
Aaryadeep <aaryadeep21122003@gmail.com>
2024-11-12 02:14:10 +0530

    using import.meta.env

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 
-export const backendUrl = process.env.VITE_BACKEND_URL;
+export const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
 const App = () => {
 
